09e2030e614ff9c712e5af0b4d318976b08d92d3
Ishaan Chowdhury <ishaanchowdhury05@gmail.com>
2026-04-25 22:18:57 +0530

    Delete frontend directory

--- frontend/store/appStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useAppStore = create((set) => ({
-  leaderboardRefresh: 0,
-  bumpLeaderboardRefresh: () =>
-    set((state) => ({ leaderboardRefresh: state.leaderboardRefresh + 1 }))
-}));
--- frontend/store/authStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-export const useAuthStore = create((set) => ({
-  token: null,
-  user: null,
-  setSession: ({ token, user }) => set({ token, user }),
-  logout: () => set({ token: null, user: null })
-}));
